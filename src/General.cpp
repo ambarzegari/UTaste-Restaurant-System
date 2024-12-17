fe3dfd8138f0480ad350ff5d8eb6f68c080b729b
am.barzegari <amir4487001@gmail.com>
2024-12-17 12:40:45 +0330

    Write general file

---
+++ src/General.cpp
@@ -0,0 +1,15 @@
+#include "General.hpp"
+
+vector<string> StringSplitter(string line)
+{
+    stringstream s(line);
+    string word;
+
+    vector<string> words;
+    
+    while (s >> word) {
+        words.push_back(word);
+    }
+    
+    return words;
+}
---
+++ src/General.hpp
@@ -0,0 +1,16 @@
+#ifndef GLOBAL_HPP
+#define GLOBAL_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> StringSplitter(string line);
+
+
+
+
+#endif
