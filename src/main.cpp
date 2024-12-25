75eb9420194aa837ac3b16811ce156c6242a7f86
am.barzegari <amir4487001@gmail.com>
2024-12-25 11:34:04 +0330

    Edit main for get data from file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char* argv[]) 
 {
     UTaste utaste;
-    utaste.GetDataFromDistrictsFile(argv[1]);
+    utaste.GetDataFromDistrictsFile(argv[2]);
+    utaste.GetDataFromResturantsFile(argv[1]);
     utaste.IoHandler();
 }
